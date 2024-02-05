bf27679cc77f4ee42afb044391cfb9d04e46a4a8
VIMQA <digenchennai@gmail.com>
2024-02-05 08:56:13 +0530

    first commit

---
+++ Task#1.py
@@ -0,0 +1,8 @@
+#Task1 - Enter User Input Details and Display the details
+
+name = input("Please Enter Your Name:")
+age = input("Please Enter Your Age:")
+roll_no = input("Please Enter Your Roll Number:")
+phone_no = input("Please Enter Your Mobile Number:")
+print("#Please find your details below#")
+print(" Name:",name,'\n',"Age :",age,'\n',"Roll Number :",roll_no,'\n',"Phone Number:",phone_no)
